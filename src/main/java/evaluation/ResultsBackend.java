d9fe3372ec56393855e6a85980d20e0580b97737
biowo <biowolf393@gmail.com>
2021-05-04 02:38:29 -0400

    Bug fixes

--- src/main/java/evaluation/ResultsBackend.java
+++ src/main/java/evaluation/ResultsBackend.java
@@ -64,6 +64,34 @@ public class ResultsBackend {
 	@FXML Label Score5C = new Label();
 	@FXML Label Score5EF = new Label();
 	
+	
+	@FXML Label comment_1_1_on = new Label();
+	@FXML Label comment_1_2_on = new Label();
+	@FXML Label comment_1_3_on = new Label();
+	@FXML Label comment_1_4_on = new Label();
+	
+	@FXML Label comment_3_1_on = new Label();
+	@FXML Label comment_3_2_on = new Label();
+	@FXML Label comment_3_3_on = new Label();
+	@FXML Label comment_3_4_on = new Label();
+	@FXML Label comment_3_5_on = new Label();
+	@FXML Label comment_3_6_on = new Label();
+	@FXML Label comment_3_7_on = new Label();
+	@FXML Label comment_3_8_on = new Label();
+	@FXML Label comment_3_9_on = new Label();
+	@FXML Label comment_3_10_on = new Label();
+	@FXML Label comment_3_11_on = new Label();
+	@FXML Label comment_3_12_on = new Label();
+	@FXML Label comment_3_13_on = new Label();
+	@FXML Label comment_3_14_on = new Label();
+	@FXML Label comment_3_15_on = new Label();
+	@FXML Label comment_3_16_on = new Label();
+	@FXML Label comment_3_17_on = new Label();
+	@FXML Label comment_3_18_on = new Label();
+	@FXML Label comment_3_19_on = new Label();
+	@FXML Label comment_3_20_on = new Label();
+	
+	
 	@FXML private void initialize() throws IOException{
 		  rankFill();
 		  ScoreW1A.setText (String.valueOf(valencia[0]));
@@ -111,6 +139,35 @@ public class ResultsBackend {
 		  Score5ABC.setText (String.valueOf(rub.CrecimientoDesarrollo()[0]));
 		  Score5C.setText (String.valueOf(rub.CrecimientoDesarrollo()[1]));
 		  Score5EF.setText (String.valueOf(rub.CrecimientoDesarrollo()[2]));
+		  
+		  if(mm.gettipo()== 0) {
+			  comment_1_1_on.setVisible(false);
+			  comment_1_2_on.setVisible(false);
+			  comment_1_3_on.setVisible(false);
+			  comment_1_4_on.setVisible(false);
+
+			  comment_3_1_on.setText ("Mediante la interacción con los estudiantes, el profesor demostró   conocimiento suficiente del tema evaluado.");
+			  comment_3_2_on.setText ("Mediante la interacción con los estudiantes, el profesor integró conceptos o experiencias de otras disciplinas para enriquecer el tema evaluado.");
+			  comment_3_3_on.setText ("El profesor resaltó la utilidad del tema para otros cursos o áreas del conocimiento.");
+			  comment_3_4_on.setText ("El profesor facilitó que los estudiantes relacionen el tema con sus experiencias personales o el mundo del trabajo.");
+			  comment_3_5_on.setText ("El profesor ejerció la docencia como un guía o facilitador de los estudiantes, mediante la comunicación e interacción con los mismos, así como en las diversas actividades docentes.");
+			  comment_3_6_on.setText ("En su interacción con los estudiantes, el profesor utilizó un lenguaje que corresponde con el nivel de los estudiantes en el curso.");
+			  comment_3_7_on.setText ("El profesor promovió la comunicación e interacción estudiante-estudiante.");
+			  comment_3_8_on.setText ("El profesor fomentó la comunicación e interacción estudiante-profesor.");
+			  comment_3_9_on.setText ( "El profesor planificó la distribución del tiempo de forma tal que los estudiantes pueden cumplir con los objetivos establecidos.");
+			  comment_3_10_on.setText ("El profesor informó a los estudiantes del tiempo estimado para responder a los mensajes, para informar sobre los resultados de los exámenes, asignaciones y otros trabajos corregidos; aunque el diseño del curso pueda incluir algo sobre esto.");
+			  comment_3_11_on.setText ("El profesor informó a los estudiantes de los resultados de las actividades realizadas por aquellos no más tarde de dos semanas de haber sido entregados durante el semestre o su equivalente en los diversos términos académicos. ");
+			  comment_3_12_on.setText ("El profesor respondió en el tiempo establecido, los mensajes, las preguntasy las consultas de los estudiantes, que se recibieron mediante el correo electrónico.");
+			  comment_3_13_on.setText ("El profesor utilizó las herramientas disponibles en la plataforma y presentó elcontenido del curso en forma organizada y coherente.");
+			  comment_3_14_on.setText ("El profesor mantuvo un clima de confianza y respeto");
+			  comment_3_15_on.setText ("El profesor facilitó la discusión para complementar, aclarar o enriquecer");
+			  comment_3_16_on.setText ("El profesor promovió la búsqueda de información o experiencias para enriqueser el conocimento.");
+			  comment_3_17_on.setText ("El profesor fomentó el pensamiento crítico.");
+			  comment_3_18_on.setText ("El profesor aportó recursos de apoyo adicionales para enriquecer el tema evaluado.");
+			  comment_3_19_on.setText ("El profesor utilizó diferentes recursos de enseñanza para facilitar el aprendizajePor ejemplo: trabajo colaborativo, vídeos, presentaciones, seminarios y conferencias a través de la web, entre otros.");
+			  comment_3_20_on.setVisible(false);
+		  }         
+		  
 	}
 	
 	public void rankFill() throws FileNotFoundException {
